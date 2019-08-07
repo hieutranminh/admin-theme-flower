303aa2c40be52a3ad34e1a8a9a5b4e349413b22c
hieutranminh <hieu.tranminhhcm@gmail.com>
2019-08-07 22:29:26 +0700

    Continue Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
-import Header from './layouts/Header'
-import Sidebar from './layouts/Sidebar'
-import Footer from './layouts/Footer'
-import PageMain from './pages/PageMain'
+import Header from './components/layouts/Header'
+import Sidebar from './components/layouts/Sidebar'
+import Footer from './components/layouts/Footer'
+import PageMain from './containers/PageMain'
+import PageProduct from './containers/ProductManagement/PageProduct'
+import MenuList from './containers/HomeManagement/Menu/MenuList'
+import Slider from './containers/HomeManagement/Slider/Slider'
+import Collection from './containers/HomeManagement/Collection/Collection'
+import BrandPartner from './containers/HomeManagement/BrandPartner/BrandPartner'
 
 function App() {
   return (
@@ -11,6 +16,11 @@ function App() {
       <Header/>
       <Sidebar/>
       <PageMain/>
+      <PageProduct/>
+      <MenuList/>
+      <Slider/>
+      <Collection/>
+      <BrandPartner/>
       <Footer/>
     </div>
   );
